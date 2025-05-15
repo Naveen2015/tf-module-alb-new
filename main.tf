f917d4c923616d051f92f82f7bf3872bbc816e40
naveenporeddy <naveenkumar9707@gmail.com>
2025-05-15 15:46:34 -0500

    alb

--- main.tf
+++ main.tf
@@ -31,12 +31,7 @@ resource "aws_lb" "main" {
   tags = { Name = "${var.name}-${var.env}-alb" }
 }
 
-# resource "aws_lb_target_group" "tg" {
-#   name     = "${var.name}-${var.env}-tg"
-#   port     = 80
-#   protocol = "HTTP"
-#   vpc_id   = var.vpc_id
-# }
+
 
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.main.arn
