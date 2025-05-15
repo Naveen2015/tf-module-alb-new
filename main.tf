7865e35379846de01ecf91bcdd9e8e0706b4b510
naveenporeddy <naveenkumar9707@gmail.com>
2025-05-15 12:07:37 -0500

    alb

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "aws_security_group" "sg" {
+  name        = "${var.name}-alb-${var.env}-sg"
+  description = "${var.name}-alb-${var.env}-sg"
+  vpc_id      = var.vpc_id
+  ingress {
+    description = "APP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = var.allow_alb_cidr
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = { Name = "${var.name}-${var.env}-sg" }
+}
+
+resource "aws_lb" "test" {
+  name               = "${var.name}-${var.env}"
+  internal           = var.internal
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg.id]
+  subnets            = var.subnets
+  tags = { Name = "${var.name}-${var.env}-alb" }
+}
+
---
+++ vars.tf
@@ -0,0 +1,7 @@
+variable "name" {}
+variable "env" {}
+variable "vpc_id" {}
+variable "tags" {}
+variable "internal" {}
+variable "subnets" {}
+variable "allow_alb_cidr" {}
