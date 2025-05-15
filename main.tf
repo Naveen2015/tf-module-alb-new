c120b6943e90117e244ffe57a4fc14573714932e
naveenporeddy <naveenkumar9707@gmail.com>
2025-05-15 15:37:04 -0500

    alb

--- main.tf
+++ main.tf
@@ -22,12 +22,35 @@ resource "aws_security_group" "sg" {
   tags = { Name = "${var.name}-${var.env}-sg" }
 }
 
-resource "aws_lb" "test" {
+resource "aws_lb" "main" {
   name               = "${var.name}-${var.env}"
   internal           = var.internal
   load_balancer_type = "application"
   security_groups    = [aws_security_group.sg.id]
   subnets            = var.subnets
   tags = { Name = "${var.name}-${var.env}-alb" }
+}
+
+# resource "aws_lb_target_group" "tg" {
+#   name     = "${var.name}-${var.env}-tg"
+#   port     = 80
+#   protocol = "HTTP"
+#   vpc_id   = var.vpc_id
+# }
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "unauthorized"
+      status_code  = "404"
+    }
+  }
 }
 
